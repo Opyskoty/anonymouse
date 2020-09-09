33e5a2048c788223ad21beb1d9bc59cb661ae36a
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-09-09 17:50:17 -0400

    update jumbotron to include icon, and form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import { Jumbotron } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBan, faCat } from "@fortawesome/free-solid-svg-icons";
 import PostList from "./PostList";
 import PostForm from "./PostForm";
+import DefaultPosts from "./DefaultPosts";
 
 function Main() {
+  const [posts, setPosts] = useState(DefaultPosts);
   return (
     <>
-    <Jumbotron>
-      <h1>AnonyMouse</h1>
-      <p>
-        <em>
-          Have you or someone you know had a rough or scary encounter with a
-          cat? We're here for you.
-        </em>
-      </p>
-      <span className="icon fa-layers fa-10x">
-        <FontAwesomeIcon icon={faCat} transform="shrink-8" />
-        <FontAwesomeIcon icon={faBan} style={{ color: "red" }} />
-      </span>
-      <div className="w-50 ml-auto mr-auto mt-5">
-        <h5>Tell us what happened:</h5>
-        <PostForm />
-      </div>
-    </Jumbotron>
-    <PostList />
+      <Jumbotron>
+        <h1>AnonyMouse</h1>
+        <p>
+          <em>
+            Have you or someone you know had a rough or scary encounter with a
+            cat? We're here for you.
+          </em>
+        </p>
+        <span className="icon fa-layers fa-10x">
+          <FontAwesomeIcon icon={faCat} transform="shrink-8" />
+          <FontAwesomeIcon icon={faBan} style={{ color: "red" }} />
+        </span>
+        <div className="w-50 ml-auto mr-auto mt-5">
+          <h5>Tell us what happened:</h5>
+          <PostForm posts={posts} setPosts={setPosts}/>
+        </div>
+      </Jumbotron>
+      <PostList posts={posts}/>
     </>
   );
 }

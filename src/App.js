060278b17a46da016341d03ee9ce428846387a9a
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-09-09 16:42:14 -0400

    remove default app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './mintbean.png';
-import './styles/index.css';
+import Routes from "./components/Routes"
+import "./App.css"
+import { Container } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>App.js</code> is the entrypoint to your app.
-        </p>
-        <h2>HAPPY HACKING!</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-    </div>
+    <Container className="text-center">
+      <Routes />
+    </Container>
   );
 }
 

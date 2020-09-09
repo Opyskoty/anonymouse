2fe0e4d5c374c5a32b1f22aa86b1125051b14bca
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-09-09 17:51:06 -0400

    add form for adding new posts

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,12 +1,53 @@
-import React from "react";
-import { Button, Form, FormGroup, Input } from "reactstrap";
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Input, Label, Col } from "reactstrap";
 
-function PostForm() {
+const INITIAL_STATE = { title: "", description: "" };
+
+function PostForm({posts, setPosts}) {
+  const [formData, setFormData] = useState(INITIAL_STATE);
+
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData((fData) => ({ ...fData, [name]: value }));
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    setPosts([...posts, {...formData}]);
+    setFormData(INITIAL_STATE);
+  };
+
   return (
     <>
-      <Form>
-        <FormGroup>
-          <Input type="textarea" name="text" id="text" />
+      <Form onSubmit={handleSubmit}>
+        <FormGroup row>
+          <Label for="title" sm={2}>
+            Title:{" "}
+          </Label>
+          <Col sm={10}>
+            <Input
+              type="text"
+              name="title"
+              id="title"
+              value={formData.title}
+              onChange={handleChange}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="description" sm={2}>
+            Description:{" "}
+          </Label>
+          <Col sm={10}>
+            <Input
+              type="textarea"
+              name="description"
+              id="description"
+              value={formData.description}
+              onChange={handleChange}
+            />
+          </Col>
         </FormGroup>
         <Button>Submit</Button>
       </Form>

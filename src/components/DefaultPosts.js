70cb1118615c1ea680d4e1458310b2dbadaab764
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-09-09 17:49:21 -0400

    update key name

--- src/components/DefaultPosts.js
+++ src/components/DefaultPosts.js
@@ -1,22 +1,22 @@
 const defaultProps = [
   {
     title: "I think a cat moved in",
-    post:
+    description:
       "Help! New humans have apparently moved into the apartment where I live in the wall. I think that they have a cat. Any words of advice?",
   },
   {
     title: "I was followed home by a cat",
-    post:
+    description:
       "The other night I was walking the city streets looking for cheese when I was followed by a cat for 17 blocks! I'm still shaken up about it",
   },
   {
     title: "I think the cat is trying to eat me",
-    post:
+    description:
       "Hello, I have been co-existing with a cat for years, and I think it's starting to develop an appetite for me. Please help I don't know what to do",
   },
   {
     title: "Where do other mice usually hang out?",
-    post: "Asking for a friend...",
+    description: "Asking for a friend...",
   },
 ];
 

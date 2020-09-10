fd58e5d0c5fe104a985b848c880948eb40efe002
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-09-10 18:18:48 -0400

    add logic for adding comments

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { v4 as uuidv4 } from "uuid";
 
-function CommentForm(){
-  return(
+const INITIAL_STATE = { comment: "" };
+
+function CommentForm({ post, posts, setPosts }) {
+  const [formData, setFormData] = useState(INITIAL_STATE);
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData((fData) => ({ ...fData, [name]: value }));
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    for (let i = 0; i < posts.length; i++) {
+      if (posts[i].id === post.id) {
+        posts[i].comments = [...posts[i].comments, { ...formData, id: uuidv4() }];
+      }
+    }
+    let updatedPosts = [...posts];
+
+    // posts.filter((p) => p.id !== post.id);
+    setPosts([...updatedPosts]);
+    setFormData(INITIAL_STATE);
+  };
+
+  return (
     <>
-    <Form>
-    <FormGroup className="mb-2 mr-sm-2 mb-sm-0 text-center">
-        <Label for="comment" className="mr-sm-2">Add a Comment</Label>
-        <Input type="text" name="comment" id="comment"/>
-      </FormGroup>
-      <Button>Add Comment</Button>
-    </Form>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0 text-center">
+          <Label for="comment" className="mr-sm-2">
+            Add a Comment
+          </Label>
+          <Input
+            type="text"
+            name="comment"
+            id="comment"
+            value={formData.comment}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <Button>Add Comment</Button>
+      </Form>
     </>
-  )
+  );
 }
 
 export default CommentForm;

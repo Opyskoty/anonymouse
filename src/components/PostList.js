986fcac0b0fd8ec0ac9d279c871b18cb548ead64
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-09-10 17:00:43 -0400

    add hot keys for post

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
-// import DefaultPosts from "./DefaultPosts";
+import React from "react";
 import PostCard from "./PostCard";
-import { Row } from "reactstrap";
+import { Row, Col } from "reactstrap";
+import { HotKeys } from "react-hotkeys";
 
-function PostList({ posts }) {
+function PostList({ posts, setPosts }) {
+  const deletePost = (postId) => {
+    const updatedPosts = posts.filter((p) => p.id !== postId);
+    setPosts(updatedPosts);
+  };
+
   return (
     <div className="PostList">
       <Row>
-        {posts.map((p) => (
-          <PostCard post={p} key={p.id} />
-        ))}
+        <Col sm="12">
+          {posts.map((p) => (
+            <HotKeys handlers={{ DELETE: () => deletePost(p.id) }} key={p.id}>
+              <PostCard
+                post={p}
+                key={p.id}
+                setPosts={setPosts}
+                posts={posts}
+                deletePost={deletePost}
+              />
+            </HotKeys>
+          ))}
+        </Col>
       </Row>
     </div>
   );

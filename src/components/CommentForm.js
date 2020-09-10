5176afb2114f8d65608479a24659af0fb4765a7e
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-09-10 16:58:31 -0400

    add comment form

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 function CommentForm(){
   return(
-    <div className="CommentForm">
-
-    </div>
+    <>
+    <Form>
+    <FormGroup className="mb-2 mr-sm-2 mb-sm-0 text-center">
+        <Label for="comment" className="mr-sm-2">Add a Comment</Label>
+        <Input type="text" name="comment" id="comment"/>
+      </FormGroup>
+      <Button>Add Comment</Button>
+    </Form>
+    </>
   )
 }
 

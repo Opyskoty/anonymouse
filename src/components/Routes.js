bac2b277eaa27217d9857173c7dad64344cbfb3d
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-09-11 09:57:58 -0400

    add redirect for invalid links

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./Home";
-import Main from "./Main"
+import Main from "./Main";
 
 function Routes() {
-
   return (
     <Switch>
       <Route exact path="/">
-        <Home/>
+        <Home />
       </Route>
       <Route exact path="/main">
         <Main />
       </Route>
+      <Route>
+        <Redirect to="/" />
+      </Route>
     </Switch>
-  )
+  );
 }
 
 export default Routes;
